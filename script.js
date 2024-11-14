50c4c4b0918d4932726b6eb8351576514fdc5265
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-14 16:21:02 +0100

    Def

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+for (let i = 1; i <= 100; i++){
+
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log (i + " FizzBuzz")
+
+    }
+    else if (i % 3 === 0){
+        console.log (i + " Fizz")
+        
+    }
+
+    else if (i % 5 === 0 ){
+        console.log (i + " Buzz")
+
+    }
+
+    else {
+        console.log (`${i} non è divisibile ne per 3 ne per 5`)
+    }   
+}
+    
